557c38ea3aae9b6a6df9f86021cd657497199db7
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-08-25 22:13:44 +0200

    Rename hotbar texture to prevent 8-slotted hotbar textures from replacing it

--- mods/sudoku_gui/init.lua
+++ mods/sudoku_gui/init.lua
@@ -13,7 +13,7 @@ minetest.register_on_joinplayer(function(player)
 	player:set_formspec_prepend(formspec)
 
 	-- Set hotbar textures
-	player:hud_set_hotbar_image("gui_hotbar.png")
+	player:hud_set_hotbar_image("sudoku_gui_hotbar.png")
 	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
 
 	player:hud_set_hotbar_itemcount(9)
